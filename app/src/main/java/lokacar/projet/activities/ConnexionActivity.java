26bd995b2edf3cb413341e88d6a25386c59762e0
Vincent Ht <hersant.vincent@gmail.com>
2018-04-27 17:24:38 +0200

    Update Project

---
+++ app/src/main/java/lokacar/projet/activities/ConnexionActivity.java
@@ -0,0 +1,16 @@
+package lokacar.projet.activities;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+
+        import lokacar.projet.R;
+
+public class ConnexionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
